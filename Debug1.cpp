c304459616a67ec830fc26d358e0eba196884a35
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-03-14 21:06:02 -0500

    Add files via upload

---
+++ Debug1.cpp
@@ -0,0 +1,18 @@
+/*
+Debug 1
+Pawelski
+3/14/2022
+Please follow the instructions on the activity sheet for what needs
+to be completed.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << Chair\n;
+    std::cout << 'Table\n';
+    std::cout << "Bed";
+    std::cout << "Dresser\n"
+    return 0;
+}
